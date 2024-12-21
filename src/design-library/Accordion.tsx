63a50a909f745cc7eea53c3656c2082749fdf3d0
Alejandro Martinez <alejandromartinez@Alejandros-Air.attlocal.net>
2024-12-20 17:13:57 -0800

    initial commit plus the creation of the accordion component from great frontend

---
+++ src/design-library/Accordion.tsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+export const Accordion = () => {
+  const content = [
+    {
+      title: "HTML",
+      description:
+        "The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser.",
+      id: "html-lang",
+    },
+    {
+      title: "CSS",
+      description:
+        "Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML.",
+      id: "css-lang",
+    },
+    {
+      title: "JavaScript",
+      description:
+        "JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS.",
+      id: "js-lang",
+    },
+  ];
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "400px",
+        flexDirection: "column",
+        gap: "2px",
+      }}
+    >
+      {content.map((item) => (
+        <Section
+          title={item.title}
+          description={item.description}
+          key={item.id}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Section = ({ title, description }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      style={{
+        width: "100%",
+      }}
+    >
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        {title}{" "}
+        <span
+          aria-hidden={true}
+          className={
+            isOpen ? "accordion-icon accordion-icon--rotated" : "accordion-icon"
+          }
+          style={{ transition: "transform 0.1s" }}
+        />
+      </button>
+      {isOpen && <p>{description}</p>}
+    </div>
+  );
+};
---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.tsx";
+import { Accordion } from "./design-library/Accordion.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <StrictMode>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/accordion" element={<Accordion />} />
+      </Routes>
+    </StrictMode>
+  </BrowserRouter>
+);
